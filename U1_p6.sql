f906c6784a6c885c3c1de839ad88f5901dd0c942
Hetvi chotara <hetvichotara@gmail.com>
2026-08-13 12:50:35 +0530

    Add files via upload

---
+++ U1_p6.sql
@@ -0,0 +1,20 @@
+set serveroutput on
+Declare
+	feet number(10,2);
+	cm number(10,2);
+	inch number(10,2);
+	meter number(10,2);
+Begin
+	feet:= &feet;
+	cm:= feet*30.38;
+	inch:=feet * 12;
+	meter:=feet * 0.3048;
+
+	Dbms_output.put_line('feet :'||feet);
+	Dbms_output.put_line('centimeter :'||cm);
+	Dbms_output.put_line('inch :'||inch);
+	Dbms_output.put_line('meter :'||meter);
+End;
+/
+set serveroutput off
+
